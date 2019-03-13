7a43e35fca61b5b40056e4b883cc5e0b33d1c079
nickchahley <nickchahley@gmail.com>
2019-03-13 17:35:58 -0400

    Migrate to Python 3
    Squashed commit of the following:
    
    commit 4b852b612577b066fdd749dbb3fee0c16b314e9a
    Author: nickchahley <nickchahley@gmail.com>
    Date:   Wed Mar 13 17:14:35 2019 -0400
    
        Update for python 3
    
    commit f029e9faacfbc22f95528191ef2408c480ecbb5f
    Author: nickchahley <nickchahley@gmail.com>
    Date:   Wed Mar 13 17:12:23 2019 -0400
    
        Bugfix: filenames not updated to formatted names
    
        eg 02-blue3.tif would be renamed to 02-blue-3.tif, but downstream would
        still try to open 02-blue3.tif, which DNE since it was renamed.
    
    commit 11c298638a0555488731c5288b576ad28a3ed77c
    Author: nickchahley <nickchahley@gmail.com>
    Date:   Wed Mar 13 16:41:21 2019 -0400
    
        Minimal conda environment
    
    commit f428a845193e99a60bbc63b8fc06502f5aa82cd1
    Author: nickchahley <nickchahley@gmail.com>
    Date:   Wed Mar 13 16:39:00 2019 -0400
    
        Exception handling for reading non-tiff files
    
    commit 8f50b2167e648acd18ca19596a20fa32b88caaec
    Author: nickchahley <nickchahley@gmail.com>
    Date:   Tue Mar 12 19:39:36 2019 -0400
    
        Vanity colors applied to error msg
    
    commit dc1c6f7ef711e0223dc5eaa0f63461bf329c83b6
    Author: nickchahley <nickchahley@gmail.com>
    Date:   Tue Mar 12 19:22:07 2019 -0400
    
        Fix scope bug and don't clobber files when renaming
    
        Bug was filenames going like 01-rgb-rgb.tif-rgb-rgb.tiff.tff
    
        Rename without clobbering. If any the color channel cannot be
        infered from any filenames, exit and report the offenders.
    
        Will still clobber any prexisting merged rgb files, if present.

--- channel_merge.py
+++ channel_merge.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """ 
 Merge greyscale single channel (r,g,b) tiffs into one rgb composite while
@@ -76,18 +76,22 @@ import argparse
 import sys
 import scipy.ndimage as ndi
 import cv2
-from libtiff import TIFF
+import tifffile as tf 
+import tkinter as tk
+from tkinter import messagebox
+from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askdirectory
 
 ### Script Info
 __author__ = 'Nick Chahley, https://github.com/nickchahley'
-__version__ = '0.2.3'
-__day__ = '2018-08-28'
+__version__ = '1.0'
+__day__ = '2019-03-12'
 
-### Command line flags/options
 def parse_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-p', '--defdir', type=str, help='Def dir for Path dialog')
+    parser.add_argument('-p', '--defdir', type=str, 
+                        help='Open the path dialog in this dir')
     parser.add_argument('-s', '--sigma', type=float, default=50., 
                         help='Sigma value for gaussian blur during illumination \
                         correction. Note: the useable range for this value is \
@@ -98,28 +102,18 @@ def parse_args():
                         help='Supress "Run Complete" popup message. Useful for \
                         batch running, since otherwise the message must be closed \
                         by user input before the script exits.')
-    parser.add_argument('--path', type=str, help='skip gui and use this path')
+    parser.add_argument('--path', type=str, 
+                        help='skip gui and load ims from this dir')
     # possible future: preprocess on/off 
     args = parser.parse_args()
     if args.path:
         args.no_popup = True 
     return args
 
-
-### Function Defs
 def popup_message(text = '', title='Message'):
-    try:
-        # Python 3.x imports
-        import tkinter as tk
-        from tkinter import messagebox
-    except ImportError:
-        # Fall back to 2.x
-        import Tkinter as tk
-        import tkMessageBox as messagebox
-
     root = tk.Tk().withdraw()  # hide the root window
-
     messagebox.showinfo(title, text)  # show the messagebo
+
 def path_dialog(whatyouwant):
     """ 
     Prompt user to select a dir (def) or file, return its path
@@ -133,29 +127,23 @@ def path_dialog(whatyouwant):
     path : str, Absolute path to file
 
     """
-    import Tkinter
-    # TODO allow multiple (shift-click) dir selections?
-    root = Tkinter.Tk()
+    # TODO allow multiple (shift-click) dir selections
+    root = tk.Tk()
     root.withdraw()
 
     opt = {}
     opt['parent'] = root
 
-    # opt['initialdir'] = './'
     opt['initialdir'] = args.defdir if args.defdir else './'
 
 
     if whatyouwant == 'folder':
-        from tkFileDialog import askdirectory
         ask_fun = askdirectory
         # dirpath will be to dir that user IS IN when they click confirm
         opt['title'] = 'Select directory containing images to merge (be IN this folder)'
 
     if whatyouwant == 'file':
-        from tkFileDialog import askopenfilename
         ask_fun = askopenfilename
-        # opt['title'] = 'Select psd file to detect peaks from'
-        # opt['filetypes'] = (('CSV files', '*.csv'), ('All files', '*.*'))
 
     path = ask_fun(**opt)
 
@@ -167,56 +155,70 @@ def path_dialog(whatyouwant):
         sys.exit(m)
 
     return path
+
 def cleanup_filenames(filenames):
-    """ 
-    replace whitespace and exclude bright field tiff files
-    """
-    def rename(filenames):
-        def format_filenames(filenames):
-            def format_trailing_nums(s):
-                import re
+    """ replace whitespace and exclude bright field tiff files """
 
-                # trim .extension
-                sp = '.'.join(s.split('.')[:-1])
-
-                # match digits at end of string
-                m = re.search('\d+$', sp)
-                if not m:
-                    # string does not end in num, ex. '01-blue.tif'
-                    new = s
-                else:
-                    # string ends in num, ex. '01-blue-2.tif'
-                    endnum = m.group()
-                    new = endnum.join(sp.split(endnum)[:-1])
-                    if new[-1] == '-':
-                        # Trailing '-', rm to avoid getting '01-blue--2.tif'
-                        new = new[:-1]
-                    ext = '.' + s.split('.')[-1]
-                    new = '-'.join((new, endnum)) + ext
-
-                # correct no seperator following prefix digits
-                # if I was good at regex this would take like zero lines
-                pfx = new.split('-')[0]
-                if not pfx.isdigit():
-                    m = re.search('^\d+', pfx)
-                    if m:
-                        mid = pfx.replace(m.group(), '')
-                        end = '-'.join(new.split('-')[1:])
-                        new = '-'.join((m.group(), mid, end))
-
-                return new
-            # replace whitespace
-            filenames = ['-'.join(f.split()) for f in filenames]
-
-            # ensure trailing digits are separated from channel_name w/ '-'
-            filenames = [format_trailing_nums(f) for f in filenames]
-            return filenames
-        for old, new in zip(filenames, format_filenames(filenames)):
+    def safe_rename(old, new):
+        if os.path.exists(new):
+            # Skip to avoid clobbering new. If this breaks the script it will
+            # be caught 
+            return
+        else:
             os.rename(old, new)
-    rename(filenames)
-    filenames = [f for f in filenames if '-bf' not in f]
-    filenames.sort()
-    return filenames
+
+    def format_filenames(filenames):
+
+        def format_trailing_nums(s):
+            import re
+
+            # trim .extension
+            sp = '.'.join(s.split('.')[:-1])
+
+            # match digits at end of string
+            m = re.search('\d+$', sp)
+            if not m:
+                # string does not end in num, ex. '01-blue.tif'
+                new = s
+            else:
+                # string ends in num, ex. '01-blue-2.tif'
+                endnum = m.group()
+                new = endnum.join(sp.split(endnum)[:-1])
+                if new[-1] == '-':
+                    # Trailing '-', rm to avoid getting '01-blue--2.tif'
+                    new = new[:-1]
+                ext = '.' + s.split('.')[-1]
+                new = '-'.join((new, endnum)) + ext
+
+            # correct no seperator following prefix digits
+            # if I was good at regex this would take like zero lines
+            pfx = new.split('-')[0]
+            if not pfx.isdigit():
+                m = re.search('^\d+', pfx)
+                if m:
+                    mid = pfx.replace(m.group(), '')
+                    end = '-'.join(new.split('-')[1:])
+                    new = '-'.join((m.group(), mid, end))
+
+            return new
+        # replace whitespace
+        filenames = ['-'.join(f.split()) for f in filenames]
+
+        # ensure trailing digits are separated from channel_name w/ '-'
+        filenames = [format_trailing_nums(f) for f in filenames]
+        return filenames
+
+    # Rename the files to be compliant with the format <id>-channel[-num].tif
+    fmt_filenames = format_filenames(filenames)
+    for old, new in zip(filenames, fmt_filenames):
+        safe_rename(old, new)
+
+
+    # Exclude brightfield files from downstream processing
+    fmt_filenames = [f for f in fmt_filenames if '-bf' not in f]
+    fmt_filenames.sort()
+    return fmt_filenames
+
 def group_images(filenames):
     """ 
     Return a dict containing image numbers as keys and a list of their
@@ -244,70 +246,78 @@ def group_images(filenames):
     channels = dict(zip(nums, channels))
 
     return channels
-def channel_combos(files):
-    """ 
-    Infer channel colors from names and return a dict with the names of all
-    files for each color
+
+def tiffs_iterate_combos(channels):
+    """ Ret dict with key for each image number make all possible rgb
+    combinations. 
 
     In
     ---
-    files : list of strings
-
-    Out
-    ---
-    combos : list of tuples, each tuple is one combo of rgb channels sorted
-        in order of (r,g,b). Imagemagick will expect this order.
-    """
-    colors = {'r' : [],
-              'g' : [],
-              'b' : []}
-
-    # interrogate channel color from filename: look at first letter 
-    # and assume r* = red, etc
-    for f in files:
-        # get the first letter of word following the img num prefix ('\d*-')
-        c = f.split('-')[1].lower()[0]
-        if c is 'r':
-            colors['r'].append(f)
-        elif c is 'g':
-            colors['g'].append(f)
-        elif c is 'b':
-            colors['b'].append(f)
-
-    # choose one item from each list, making all possible combos
-    combos = [p for p in itertools.product(*colors.values())]
-
-    # reverse alpha sort each tuple so that order is rgb -- imagemagick assumes
-    # this order
-    combos = [sorted(t, reverse=True) for t in combos]
-
-    # List of tuples, each tuple is one combo of rgb channels
-    return combos
-def tiffs_iterate_combos(d):
-    """ 
-    Ret dict with key for each image number make all possible rgb combinations. 
-
-    In
-    ---
-    d : dict of dicts of lists
-        d = { <##> : { 
-            r : [filenames], g : [], b : [] } 
-            }
+    channels : dict of dicts of lists
+        { <id> : 
+            { r : [filenames], g : [], b : [] } 
+        }
+    ^ no that's wrong. I'm actually feeding it:
+        { <id> : [<filenames>] }
 
     Out
     ---
     imgs : dict of lists of tuples. Each tuple is one rgb combination. Each 
         list is all tuples for a given image number.
     """
+
     imgs = {}
-    for k, v in d.iteritems():
-        imgs[k] = channel_combos(v)
+    bad_files = []
+    for im_id, filenames in channels.items():
+
+        colors = {'r' : [],
+                  'g' : [],
+                  'b' : []}
+
+        # interrogate channel color from filename: look at first letter 
+        # and assume r* = red, etc
+        for f in filenames:
+            try:
+                # get the first letter of word following the img num prefix ('\d*-')
+                c = f.split('-')[1].lower()[0]
+                if c is 'r':
+                    colors['r'].append(f)
+                elif c is 'g':
+                    colors['g'].append(f)
+                elif c is 'b':
+                    colors['b'].append(f)
+            except IndexError as e:
+                bad_files.append(f)
+
+        # choose one item from each list, making all possible combos
+        combos = [p for p in itertools.product(*colors.values())]
+
+        # reverse alpha sort each tuple so that order is rgb 
+        combos = [sorted(t, reverse=True) for t in combos]
+
+        imgs[im_id] = combos 
+
+
+    # bad_files is an exit flag is so that if multiple uninferable colors
+    # exist, all of them are printed, and the user doesn't end up fixing a
+    # conflict only to run it again and find there was a second, previously
+    # unreported conflict they now need to go back and resolve.
+    if len(bad_files) > 0:
+        msg = (
+            bcolors.WARNING + 'Error: ' + bcolors.ENDC +
+            'Unable to infer color channel from {n} filename(s)\n'
+            'This is likely due to the filename not containing any '
+            '"-" separators from a conflict when trying to coerce the '
+            'image names to the format "<id>-channel[-opt]". You will '
+            'need to manually resolve any name conflicts.').format(
+                n=len(bad_files))
+        sys.exit(msg + bcolors.FAIL 
+                 + "\nAborting script due to uninferable color channels")
+        # sys.exit(msg + "\nAborting script due to uninferable color channels")
     
-    # dict of list of tuples
+    # dict( list( tuple ) )
     return imgs
 
-## Resturaunt Nouveau System
-# Did I mess up using constant as filter mode?
 def preproc_imgs(imgs, sigma, oudtdir='preproc', mode='nearest'):
     """ 
     Hastily commented preprocessing. 'uids' is a dumb name for this dict.
@@ -317,7 +327,7 @@ def preproc_imgs(imgs, sigma, oudtdir='preproc', mode='nearest'):
     def get_uids(imgs):
         # Get a unique id for each distinct len3 list of r,g,b files
         uids = {}
-        for k, imls in imgs.iteritems():
+        for k, imls in imgs.items():
             if len(imls) == 1:
                 if type(imls[0]) is list:
                     # then we have a len1 list containing another list for some reason
@@ -357,10 +367,10 @@ def preproc_imgs(imgs, sigma, oudtdir='preproc', mode='nearest'):
     # For each set of 3 channel filenames, read each image, preform
     # illumination correction, and stack them together into an rgb image.
     rgb = {}
-    for uid, imls in uids.iteritems():
+    for uid, imls in uids.items():
 
         # List of greyscale channel ims : r,g,b
-        ims = [tiffread(f) for f in imls] 
+        ims = [tiffread(f) for f in imls]
 
         # Guassian blur bg subtraction for each channel
         ims_corr = [illum_correction(x, sigma, mode) for x in ims]
@@ -375,49 +385,72 @@ def preproc_imgs(imgs, sigma, oudtdir='preproc', mode='nearest'):
             print('B: %s' % str(ims_corr[2].shape))
     
     return rgb
+
 def outfile_names(rgb, suffix='rgb', ext='.tif'):
     """ Take dict of num : rgb im and return outfilename : rgb num
     """
-
-    for k in rgb.keys():
-        ks = k.split('-')
+    outfile_rgb = {}
+    for im_id, data in rgb.items():
+        ks = im_id.split('-')
         # if im num is of fmt '01-2' make name '01-suffix-2.ext'
         if len(ks) == 2:
             fname = '-'.join((ks[0], suffix, ks[-1])) + ext
         else:
-            fname = '-'.join((k, suffix)) + ext
-        rgb[fname] = rgb.pop(k)
-    return rgb
+            fname = '-'.join((im_id, suffix)) + ext
+        outfile_rgb[fname] = data
+    return outfile_rgb
+
 def tiffread(f):
+    """ Read .tiff into numpy.ndarray
+    Might be simpler than w/ libtiff. I'll note the catches I used to need 
+    w/ libtiff just in case:
+        - if reading a list, read a list of gs tifs and np.dstack (will do
+        similar here)
+
+    In
+    --
+    f : str or list, filename if gs or rgb, filenames if list of gs
+
+    Out
+    ---
+    im : numpy.ndarray
     """
-    Return a single array if given a filename, and a rgb stack if fed a len 3
-    list of filenames.
+    try:
+        if type(f) is str:
+            # single image
+            im = tf.imread(f)
+            return im
 
-    f : str or list (len 3), filename(s) to be read.
+        elif type(f) is list and len(f) == 3:
+            # return rgb stack
+            f.sort(reverse=True) # so r, g, b
+            ims = [tf.imread(x) for x in f]
+            return np.dstack(ims)
+        else:
+            raise ValueError("f must be a string or list of 3 strings")
+    except tf.tifffile.TiffFileError:
+        sys.exit(bcolors.FAIL+'ERROR: %s is not a tiff file. Exiting' %f)
 
-    ret : 2d or 3d ndarray
-    """
-    if type(f) is str:
-        # single image
-        tif = TIFF.open(f, mode='r')
-        return tif.read_image()
-
-    elif type(f) is list and len(f) == 3:
-        # return rgb stack
-        f.sort(reverse=True) # so r, g, b
-        tif = [TIFF.open(x, mode='r') for x in f]
-        ims = [t.read_image() for t in tif]
-        return np.dstack(ims)
-    else:
-        raise ValueError("f must be a string or list of 3 strings")
 def tiffwrite(filename, im):
-    tif = TIFF.open(filename, mode='w')
-    # Write as a composite r,g,b if it looks like one
-    if len(im.shape) == 3 and im.shape[-1] == 3:
-        tif.write_image(im, write_rgb = True)
-    else:
-        tif.write_image(im)
+    """ Write numpy.ndarray to tif.
+    Might be simpler than w/ libtiff. I'll note the catches I used to need 
+    w/ libtiff just in case:
+        - need to distinguish b/t greyscale and rgb (write_rgb=True if shape 3)
 
+    filename : str
+    im : numpy.ndarray
+    """
+    tf.imwrite(filename, im)
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 ### Main 
 def main():
@@ -429,12 +462,13 @@ def main():
     os.chdir(path)
 
     # Filename String Manipulations
+    print('Formatting filenames...')
     filenames = cleanup_filenames(glob("*.tif"))
     channels = group_images(filenames)
+    imgs = tiffs_iterate_combos(channels)
 
     # Image Processing
     print('Processing images...')
-    imgs = tiffs_iterate_combos(channels)
     rgb = preproc_imgs(imgs, sigma = args.sigma)
     rgb = outfile_names(rgb)
 
@@ -443,7 +477,7 @@ def main():
     if not os.path.exists(args.outdir):
         os.makedirs(args.outdir)
     
-    for fname, im in rgb.iteritems():
+    for fname, im in rgb.items():
         tiffwrite('/'.join((args.outdir, fname)), im)
 
     if args.path:
@@ -459,3 +493,4 @@ if __name__ == '__main__':
     main()
     if args.no_popup == False:
         popup_message('Run complete')
+
